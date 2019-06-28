47438c53481eedfa0ec8c2f3beec6123ef20df7c
jbenet <juan@benet.ai>
2019-06-28 14:17:00 +0200

    data formats for dht tracer

---
+++ datafmts/vis-events.go
@@ -0,0 +1,66 @@
+package datafmts
+
+// data types for the vis part.
+
+type QueryRunnerTable struct {
+  Queries []QueryRow
+}
+
+type QueryRow struct {
+  Key         string
+  Type        string
+  RunnerState QueryRunnerState
+}
+
+type QueryRunnerState struct {
+  PeersSeen      int
+  PeersQueried   int
+  PeersDialed    int
+  PeersToQuery   int
+  PeersRemaining int
+  Result         int // number of records
+  RateLimit      RateLimit
+  Result         QueryResult
+  StartTime      string
+  CurrTime       string
+  EndTime        string
+}
+
+type QueryResult struct {
+  Success     bool
+  FoundPeer   string
+  CloserPeers int
+  FinalSet    int
+  QueriedSet  int
+}
+
+type QueryPeerRow struct {
+  QueryID       string
+  QueryOrder    int // sequentially counting up rows.
+  PeerID        string
+  XORDistance   int
+  Hops          int
+  Spans         []Span
+  TotalDuration string
+
+  // results data
+  Records int // if any records received
+
+  CloserPeersRecv int // total new peers received
+  CloserPeersNew  int // number of peers which were new
+
+  ProviderPeersRecv int // total new peers received
+  ProviderPeersNew  int // number of peers which were new
+}
+
+type Span struct {
+  Type     string // Dial, Request, etc.
+  Start    string // timestamp
+  End      string // timestamp
+  Duration string // time duration
+}
+
+type RateLimit struct {
+  Capacity int
+  Length   int
+}
---
+++ datafmts/vis-react.jsx
@@ -0,0 +1,20 @@
+
+<QueryTable>
+  <QuerySection>
+    <QueryRow />
+    <QueryPeerRow />
+    <QueryPeerRow />
+    <QueryPeerRow />
+    <QueryPeerRow />
+    ...
+  </QuerySection>
+  <QuerySection>
+    <QueryRow />
+    <QueryPeerRow />
+    <QueryPeerRow />
+    <QueryPeerRow />
+    <QueryPeerRow />
+    ...
+  </QuerySection>
+  ...
+</QueryTable>
